7e74081b770f518925026b985558245768567b39
Darron Moraes <darron.dev@gmail.com>
2024-12-14 03:45:03 +0530

    initial commit

---
+++ src/main/java/in/nineteen96/jwt_demo/JwtAuthDemoApplication.java
@@ -0,0 +1,13 @@
+package in.nineteen96.jwt_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtAuthDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtAuthDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/nineteen96/jwt_demo/JwtAuthDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package in.nineteen96.jwt_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtAuthDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
