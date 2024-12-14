917669e84e096ce59dd3fa5502c22815940ebb74
Darron Moraes <darron.dev@gmail.com>
2024-12-14 08:46:30 +0530

    feat: add user entity and repository, remove validate tables/schemas

---
+++ src/main/java/in/nineteen96/jwt_demo/entity/User.java
@@ -0,0 +1,51 @@
+package in.nineteen96.jwt_demo.entity;
+
+import in.nineteen96.jwt_demo.utils.Role;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.UuidGenerator;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @UuidGenerator
+    private String id;
+    private String name;
+    private String email;
+    private String password;
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+
+}
---
+++ src/main/java/in/nineteen96/jwt_demo/utils/Role.java
@@ -0,0 +1,5 @@
+package in.nineteen96.jwt_demo.utils;
+
+public enum Role {
+    USER, ADMIN, DEVELOPER
+}
