ffdb154689d61d6944179a02b0fc94b0e029b665
Darron Moraes <darron.dev@gmail.com>
2024-12-15 00:37:34 +0530

    feat: jwt token implementation and security config update

---
+++ src/main/java/in/nineteen96/jwt_demo/auth/JwtAuthenticationFilter.java
@@ -0,0 +1,77 @@
+package in.nineteen96.jwt_demo.auth;
+
+import in.nineteen96.jwt_demo.service.UserService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@Slf4j
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+
+    private final JwtTokenProvider tokenProvider;
+
+    private final UserService userService;
+
+    public JwtAuthenticationFilter(JwtTokenProvider tokenProvider, UserService userService) {
+        this.tokenProvider = tokenProvider;
+        this.userService = userService;
+    }
+
+
+    @Override
+    protected void doFilterInternal(
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain
+    ) throws ServletException, IOException {
+        System.out.println("getting authorization header");
+//        log.info("attempt to get authorization header");
+        final String authorizationHeader = request.getHeader("Authorization");
+        final String token;
+
+        if (authorizationHeader == null || !authorizationHeader.startsWith("Bearer ")) {
+            System.out.println("authorization header not found");
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        token = authorizationHeader.substring(7);
+        final String userEmail;
+
+         userEmail = tokenProvider.extractUserEmail(token);
+        // todo extract the user email from jwt token
+
+        if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            System.out.println("username found in token and security context is null...");
+            System.out.println("loading user by username");
+            UserDetails userDetails = this.userService.loadUserByUsername(userEmail);
+            System.out.println("validating token...");
+            if (tokenProvider.isTokenValid(token, userDetails)) {
+                System.out.println("token is valid");
+                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+                        userDetails,
+                        null,
+                        userDetails.getAuthorities()
+                );
+                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+                System.out.println("security context updated");
+            }
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
---
+++ src/main/java/in/nineteen96/jwt_demo/config/SecurityConfig.java
@@ -0,0 +1,73 @@
+package in.nineteen96.jwt_demo.config;
+
+import in.nineteen96.jwt_demo.auth.JwtAuthenticationFilter;
+import in.nineteen96.jwt_demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Autowired
+    private JwtAuthenticationFilter jwtAuthFilter;
+
+    @Autowired
+    private UserService userService;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(
+                        auth -> auth.requestMatchers(
+                                "/auth/welcome", "/auth/register", "/auth/login"
+                        ).permitAll()
+                )
+                .authorizeHttpRequests(auth -> auth.requestMatchers(
+                        "/v1/bookings/**"
+                        ).authenticated()
+                )
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authenticationProvider(authenticationProvider())
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userService);
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+
+        return authenticationProvider;
+    }
+
+//    @Bean
+//    public UserDetailsService userDetailsService() {
+//        return new UserService();
+//    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+}
